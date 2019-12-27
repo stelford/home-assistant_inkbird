b32bd379d7d0a94c0d968ef51d81247fefb04606
Stef T <stef@ummon.com>
2019-12-27 00:30:02 -0500

    Total rework
    Pretty much. The sensor now works by scanning for the btle
    broadcast instead of trying to inquire via the characteristics.
    This allows better power/battery usage, but it also means that
    we can get battery now, which is nice :)

--- README.md
+++ README.md
@@ -5,15 +5,25 @@ from install to install). In my install, this would be at
 /config/custom_components/inkbird. 
 
 Change your /config/configuration.yaml to have something like;
+NOTE: Configuration breaking change. The devices are now under
+a subsection.
 
 ```
 sensor:
   - platform: inkbird
-    mac: '90:e2:02:9b:45:3a'
-    name: 'Cians Room'
-    monitored_conditions:
-      - temperature
-      - humidity
+    devices:
+      - mac: '90:e2:02:9b:45:3a'
+        name: 'Cians Room'
+        monitored_conditions:
+          - temperature
+          - humidity
+          - battery
+      - mac: '90:e2:02:9b:4b:64'
+        name: 'Kats Room'
+        monitored_conditions:
+          - temperature
+          - humidity
+          - battery
 ```
 
 Obviously, the MAC and name you will change to tastes. The MAC you 
@@ -21,14 +31,16 @@ can find by using the scan.py inside helper_scripts. You can also
 test in a 'once off' fashion by using the test_btle.py with the
 changed MAC inside it.
 
-There are a few caveats, of course;
+With the rework, almost all of the caveats in the first release are
+now obsoleted. Every time a scan_interval is hit (usually 30s or such)
+then the Inkbird.Updater will scan the btle for any broadcasts for 10s.
+The Inkbird sends out a broadcast every 10s as well. This means that
+from time to time, we won't get lucky and listen at the right time.
 
-1) ~~I have not tested with multiple sensors yet~~ Tested fine :)
-2) the sensor itself has a pretty nasty 'drop off' signal, at
-   around 25 feet (in my experience). Bluetooth is not meant
-   for long range
-3) The code is based on the Xiaomi Sensor, and as such, it double
-   polls if you have both temperature and humidity in there. The
-   Inkbird mini really doesn't like getting "Spammed", so I put in
-   a throttle for 60s. Reducing that will not help but, it's your
-   choice to make.
+That said, there is no more btle connections happening (as it's using
+broadcasted data from the devices only now). It's also vastly more
+power efficient. Battery support has been added as well. Thanks to
+Canadian winters, negative temperatures should be registered accordingly.
+
+In short, I am now concluding that the device is "100% supported" and
+there probably won't be any more check-ins after this one.
--- sensor.py
+++ sensor.py
@@ -1,9 +1,15 @@
+import asyncio
+from collections import OrderedDict
 from datetime import timedelta, datetime
 import logging
+from random import randint
+import re
 from struct import unpack
+import signal
+import time
 
 from bluepy import btle
-from bluepy.btle import BTLEException
+from bluepy.btle import BTLEException, Scanner, DefaultDelegate
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import (
@@ -15,23 +21,32 @@ from homeassistant.util import Throttle
 import voluptuous as vol
 
 
-_LOGGER = logging.getLogger(__name__)
-
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
-
+DOMAIN = 'Inkbird'
 DEFAULT_NAME = 'Inkbird'
-
+_LOGGER = logging.getLogger(__name__)
+SCAN_INTERVAL = timedelta(seconds=60)
 # Sensor types are defined like: Name, units
 SENSOR_TYPES = {
+    'updater': [None, 'Updater', None],
     'temperature': [DEVICE_CLASS_TEMPERATURE, 'Temperature', '°C'],
     'humidity': [DEVICE_CLASS_HUMIDITY, 'Humidity', '%'],
+    'battery': [DEVICE_CLASS_BATTERY, 'Battery', '%'],
+}
+
+STATE_ATTR_TEMPERATURE = "temperature"
+STATE_ATTR_HUMIDITY = "humidity"
+STATE_ATTR_BATTERY = "battery"
+
+ENTITY_ITEM = {
+    vol.Required(CONF_MAC): cv.string,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_MONITORED_CONDITIONS, default=list(SENSOR_TYPES)):
+        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
 }
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_MAC): cv.string,
-    vol.Optional(CONF_MONITORED_CONDITIONS, default=list(SENSOR_TYPES)):
-        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Required('devices'):
+        vol.All(cv.ensure_list, [OrderedDict(ENTITY_ITEM)])
 })
 
 
@@ -39,59 +54,50 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Inkbird thermostat."""
     _LOGGER.info(">>> The inkbird component is ready!")
 
-    dataIn = InkbirdDataRequest(config.get(CONF_MAC))
-    devs = [] 
+    inkbird_devices = []
 
-    for parameter in config[CONF_MONITORED_CONDITIONS]:
-        name = SENSOR_TYPES[parameter][1]
-        uom = SENSOR_TYPES[parameter][2]
+    for device in config['devices']:
+        for parameter in device['monitored_conditions']:
+            name = SENSOR_TYPES[parameter][1]
+            uom = SENSOR_TYPES[parameter][2]
 
-        prefix = config.get(CONF_NAME)
-        if prefix:
-            name = "{} {}".format(prefix, name)
+            prefix = device['name']
+            if prefix:
+                name = "{} {}".format(prefix, name)
+            entity_name = re.sub(' ', '_', name.lower())
 
-        devs.append(InkbirdSensor(dataIn, parameter, uom, name))
+            if parameter == "temperature":
+                inkbird_devices.append( InkbirdThermalSensor(device['mac'], uom, name, entity_name) )
+            elif parameter == "humidity":
+                inkbird_devices.append( InkbirdHumiditySensor(device['mac'], uom, name, entity_name) )
+            else:
+                inkbird_devices.append( InkbirdBatterySensor(device['mac'], uom, name, entity_name) )
 
-    add_entities(devs)
+    inkbird_devices.append( InkbirdUpdater(hass, inkbird_devices) )
+    add_entities(inkbird_devices, True)
 
 
-class InkbirdDataRequest(object):
+class InkbirdUpdater(Entity):
 
-    def __init__(self, mac):
+    entity_id = "inkbird.updater"
+
+    def __init__(self, hass, inkbird_devices):
         """Initialize the thermometer."""
-        self.mac = mac
-        self.temperature = None
-        self.humidity = None
-
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self):
-        """Update the data from the thermometer."""
-        try:
-            dev = btle.Peripheral(self.mac)
-            readings = dev.readCharacteristic(40)
-        except BTLEException as error:
-            _LOGGER.error("Error occurred while fetching data: %s", error)
-            return False
-        _LOGGER.debug("raw readings is %s", readings)
-        temperature, humidity = unpack("<HH",readings[0:4])
-        self.temperature = temperature/100
-        self.humidity = humidity/100
-        return True
-
-class InkbirdSensor(Entity):
-    """Representation of a Inkbird Sensor."""
-
-    def __init__(self, dataIn, parameter, uom, name):
-        """Initialize the sensor."""
-        self.dataIn = dataIn
-        self.data = []
-        self._unit_of_measurement = uom
-        self._name = name
-        self.parameter = parameter
-        self._current_temperature = None
-        self._current_humidity = None
-        self.update()
+        Entity.__init__(self)
+        self._name = 'Inkbird Updater'
         self._state = None
+        self._mac = None
+        self.hass = hass
+        self.scanner = Scanner()
+        self.scanner.clear()
+        self.scanner.start()
+        self.no_results_counter = 0
+        self.inkbird_devices = inkbird_devices
+
+    @property
+    def mac(self):
+        """Return the mac of the sensor."""
+        return self._mac
 
     @property
     def name(self):
@@ -104,26 +110,6 @@ class InkbirdSensor(Entity):
         return self._state
 
     @property
-    def unit_of_measurement(self):
-        """Return the units of measurement."""
-        return self._unit_of_measurement
-
-    @property
-    def current_temperature(self):
-        """Return the state of the sensor."""
-        return self._current_temperature
-
-    @property
-    def current_humidity(self):
-        """Return the state of the sensor."""
-        return self._current_humidity
-
-    @property
-    def temperature_unit(self):
-        """Return the unit of measurement which this thermostat uses."""
-        return TEMP_CELSIUS
-
-    @property
     def should_poll(self):
         """Return the name of the sensor."""
         _LOGGER.debug("Should_Poll called")
@@ -131,9 +117,228 @@ class InkbirdSensor(Entity):
 
     def update(self):
         """Get the latest data and use it to update our sensor state."""
-        _LOGGER.debug("calling update .. ")
-        update = self.dataIn.update()
-        if update == True:
-            self.data = getattr(self.dataIn,self.parameter)
-            self._state = self.data
+        _LOGGER.debug("UPDATE called")
+        _LOGGER.debug(f"scanner here is {self.scanner}")
+
+        # The btle on my raspberry pi 4 seems to go MIA
+        if self.no_results_counter >= 5:
+            _LOGGER.error("Btle went away .. restarting entire btle stack")
+            self.scanner = Scanner()
+            self.scanner.clear()
+            self.scanner.start()
+            self.no_results_counter = 0
+
+        try:
+            self.scanner.process(timeout=8.0)
+        except:
+            e = sys.exc_info()[0]
+            _LOGGER.error(f" Exception occoured during scanning: {e}")
+        results = self.scanner.getDevices()
+        _LOGGER.debug(f"got results {results}")
+        for dev in results:
+            self.handleDiscovery(dev)
+
+        # if we have no results at all, the scanner may have gone MIA.
+        # it happens apparently. So, let's count upto 5 and then, if it
+        # still happens, restart/refresh the btle stack.
+        # any results though will reset the btle 'MIA counter' to 0
+        if not any(results):
+            self.no_results_counter += 1
+        else:
+            self.no_results_counter = 0
+            self.scanner.clear()
+
+        self._state = []
         return True
+
+    def handleDiscovery(self, dev):
+        _LOGGER.debug(f"Discovered device {dev.addr}")
+        _LOGGER.debug("Device {} ({}), RSSI={} dB".format(dev.addr, dev.addrType, dev.rssi))
+        for (adtype, desc, value) in dev.getScanData():
+            _LOGGER.debug("[%s]  %s = %s" % (adtype, desc, value))
+            if adtype == 255:
+                humidity = "%2.2f" % (int(value[6:8]+value[4:6], 16)/100)
+                #temperature = "%2.2f" % (int(value[2:4]+value[:2], 16)/100)
+                temperature = int(value[2:4]+value[:2], 16)
+                temperature_bits = 16
+                if temperature & (1 << (temperature_bits-1)):
+                    temperature -= 1 << temperature_bits
+                temperature = "%2.2f" % (temperature / 100)
+                battery = int(value[14:16], 16)
+                _LOGGER.debug(self.inkbird_devices)
+                for device in self.inkbird_devices:
+                    _LOGGER.debug(f" dev addr is {dev.addr} and mac is {device.mac}")
+                    _LOGGER.debug(f" --> {temperature} - {humidity} - {battery} ")
+                    if dev.addr == device.mac:
+                        _LOGGER.debug(f" dev addr is {dev.addr} and mac is {device.mac} with parameter of {device.parameter}")
+                        old_state = self.hass.states.get(f"sensor.{device.entity_name}")
+                        if old_state:
+                            attrs = old_state.attributes
+                        else:
+                            attrs = None
+
+                        if device.parameter == "temperature":
+                            _LOGGER.debug(f" >>>> updating device {device.mac} with {temperature}")
+                            device.temperature = temperature
+                            device._state = temperature
+                            #self.hass.states.set(f"sensor.{device.entity_name}", temperature, attrs)
+                        elif device.parameter == "humidity":
+                            _LOGGER.debug(f" >>>> updating device {device.mac} with {humidity}")
+                            device.humidity = humidity
+                            device._state = humidity
+                            #self.hass.states.set(f"sensor.{device.entity_name}", humidity, attrs)
+                        else:
+                            _LOGGER.debug(f" >>>> updating device {device.mac} with {battery}")
+                            device.battery = battery
+                            device._state = battery
+                            #self.hass.states.set(f"sensor.{device.entity_name}", battery, attrs)
+
+
+
+class InkbirdThermalSensor(Entity):
+    """Representation of a Inkbird Sensor."""
+
+    def __init__(self, mac, uom, name, entity_name):
+        """Initialize the sensor."""
+        Entity.__init__(self)
+        self._device_class = DEVICE_CLASS_TEMPERATURE
+        self._mac = mac
+        self._unit_of_measurement = uom
+        self._name = name
+        self._entity_name = entity_name
+        self.parameter = "temperature"
+        self._state = None
+        self.temperature = None
+
+    @property
+    def mac(self):
+        """Return the mac of the sensor."""
+        return self._mac
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def entity_name(self):
+        """Return the entity name of the sensor."""
+        return self._entity_name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the units of measurement."""
+        return self._unit_of_measurement
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement which this thermostat uses."""
+        return TEMP_CELSIUS
+
+    @property
+    def state_attributes(self):
+        """Return the state attributes of the sun."""
+        return {
+            STATE_ATTR_TEMPERATURE: self.temperature
+        }
+
+
+class InkbirdHumiditySensor(Entity):
+    """Representation of a Inkbird Sensor."""
+
+    def __init__(self, mac, uom, name, entity_name):
+        """Initialize the sensor."""
+        Entity.__init__(self)
+        self._device_class = DEVICE_CLASS_HUMIDITY
+        self._mac = mac
+        self._unit_of_measurement = uom
+        self._name = name
+        self._entity_name = entity_name
+        self.parameter = "humidity"
+        self._state = None
+        self.humidity = None
+
+    @property
+    def mac(self):
+        """Return the mac of the sensor."""
+        return self._mac
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def entity_name(self):
+        """Return the entity name of the sensor."""
+        return self._entity_name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the units of measurement."""
+        return self._unit_of_measurement
+
+    @property
+    def state_attributes(self):
+        """Return the state attributes of the sun."""
+        return {
+            STATE_ATTR_HUMIDITY: self.humidity
+        }
+
+
+class InkbirdBatterySensor(Entity):
+    """Representation of a Inkbird Sensor."""
+
+    def __init__(self, mac, uom, name, entity_name):
+        """Initialize the sensor."""
+        Entity.__init__(self)
+        self._device_class = DEVICE_CLASS_BATTERY
+        self._mac = mac
+        self._unit_of_measurement = uom
+        self._name = name
+        self._entity_name = entity_name
+        self.parameter = "battery"
+        self._state = None
+        self.battery = None
+
+    @property
+    def mac(self):
+        """Return the mac of the sensor."""
+        return self._mac
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def entity_name(self):
+        """Return the entity name of the sensor."""
+        return self._entity_name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the units of measurement."""
+        return self._unit_of_measurement
+
+    @property
+    def state_attributes(self):
+        """Return the state attributes of the sun."""
+        return {
+            STATE_ATTR_BATTERY: self.battery
+        }
